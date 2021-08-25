439cd5933c49062ee94045bf80109a462b712634
Steve Brunelli <brunes46@gmail.com>
2021-08-25 10:55:52 -0500

    added json placeholder to pull robots from API as componentDidMount. Also added if statement to display loading text if app takes long to load

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import CardList from './components/CardList/card-list.component.jsx';
 import SearchBox from './components/SearchBox/searchBox.component.jsx';
-import { robots } from './robots';
 
 import './App.css';
 
@@ -10,29 +9,37 @@ class App extends Component {
     constructor() {
         super();
         this.state= {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {this.setState({ robots: users})});
+    }
+
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value});
-
-     
     }
 
     render () {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-        });   
+        });
 
-        return (
-            <div className="App">
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        if(this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className="App">
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
     }
 }
 
