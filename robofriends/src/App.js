ec2aab57caac41d9132f995dc82a76d204e2d5ee
Steve Brunelli <brunes46@gmail.com>
2021-08-25 10:33:23 -0500

    added search bar and state to app.js and created search functionality for search bar to render new robots on the page

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import CardList from './components/CardList/card-list.component.jsx';
+import SearchBox from './components/SearchBox/searchBox.component.jsx';
 import { robots } from './robots';
+
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state= {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value});
+
+     
+    }
+
     render () {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        });   
+
         return (
             <div className="App">
-                <CardList robots={robots}/>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots}/>
             </div>
         );
     }
